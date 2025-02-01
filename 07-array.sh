283dbe43e2c2d0473743acffaad918eacb43e191
chsurya11 <chsurya11@gmail.com>
2025-02-01 22:41:45 +0530

    data type script added

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Dhi","RRR","K")
+
+echo "First movie: ${MOVIES[0]}
+echo "Second movie: ${MOVIES[1]}
+echo "Third movie: ${MOVIES[2]}
+
+echo "All the movies are: ${MOVIES[@]}
