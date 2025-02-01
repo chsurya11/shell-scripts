cb4828086f52ad3380a5084f93d901b54a27c9f2
chsurya11 <chsurya11@gmail.com>
2025-02-01 23:00:04 +0530

    data type script added

---
+++ 08-Special-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "all variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "present working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process ID of current script: $$"
+echo "Process ID of last command in background: $!"
