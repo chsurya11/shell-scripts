964b84463d0648c315e04500f7caf5e24baffa7f
chsurya11 <chsurya11@gmail.com>
2025-02-04 00:21:04 +0530

    colours and variables added

---
+++ 12-colour_1.sh
@@ -0,0 +1,11 @@
+#!/bin/Bash
+
+echo "Hello world"
+echo -e "\e[31m Hello surya"
+echo -e "\e[32m Hello teja"
+echo -e "\e[33m hello dhruvann"
+echo -e "\e[34m hello krishna"
+echo -e "\e[35m hello geethika"
+echo -e "\e[36m hello dhru"
+echo -e "\e[37m hello tej"
+echo -e "\e[0m hello sur"
