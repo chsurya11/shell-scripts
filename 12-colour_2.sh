a7819aff6dc9b740ed2f1516d5bf46f5ed816027
chsurya11 <chsurya11@gmail.com>
2025-02-04 00:25:08 +0530

    colours and variables added

---
+++ 12-colour_2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VARIABLE(){
+    if [ $1 -ne 0 ]
+        echo -e "$2... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2.. $G SUCCESS"
+    fi
+}
+
+if [ $? -ne 0 ]
+then
+    echo -e "$R ERROR"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+else
+    echo -e "mysql is already.. $G installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "installing git"
+else
+    echo -e "GIT is already.. $G installed"
+fi
