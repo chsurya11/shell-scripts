8f4a8fde86c61b35f4e2a5b6142848f8511b9dc5
chsurya11 <chsurya11@gmail.com>
2025-02-04 13:27:44 +0530

    data type script added

---
+++ 13-logs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2... $R FAILURE $N"
+    else
+        echo -e "$2... $G SUCCESS"
+    fi
+}
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: you must have a sudo access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql0 &>>$LOG_FILE
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "Installing..MYSQL"
+else
+    echo -e "MYSQL is already ..$Y installed"
+fi
