3f792c3a903a0f3445aa6691946089bbe4e53a4c
chsurya11 <chsurya11@gmail.com>
2025-02-25 16:22:43 +0530

    backup code added

--- 18-backup.sh
+++ 18-backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -8,46 +7,37 @@ N="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14} #if user is not passing no. of days .we are taking 14days as default
+DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
 
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2... $G SUCCESS $N"
-    fi
-}
-
 USAGE(){
-    echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
+    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }
 
-mkdir -p /home/ec2-user/shellscript-logs/
+mkdir -p /home/ec2-user/shellscript-logs
 
 if [ $# -lt 2 ]
 then
     USAGE
 fi
 
-if [ ! -d $SOURCE_DIR ]
+if [ ! -d "$SOURCE_DIR" ]
 then
-    echo -e "$SOURCE_DIR Does not exit..Please check"
+    echo -e "$SOURCE_DIR Does not exist...Please check"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR ]
+if [ ! -d "$DEST_DIR" ]
 then
-    echo -e "$DEST_DIR Does not exit...Please check"
+    echo -e "$DEST_DIR Does not exist...Please check"
     exit 1
-fi 
+fi
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
